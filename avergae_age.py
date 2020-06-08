88007a8405c7ca35ecd507255facf3bd4f306948
zeropaul <landerspaulzero@gmail.com>
2020-06-07 19:20:40 -0500

    avergae_age

--- avergae_age.py
+++ avergae_age.py
@@ -145,28 +145,28 @@ studens5 = [
     {'edad': 11, 'sexo': 'F'},
 ]
 studens6 = [
-    {'edad': 11, 'sexo': 'M'},
-    {'edad': 10, 'sexo': 'F'},
-    {'edad': 13, 'sexo': 'M'},
-    {'edad': 11, 'sexo': 'F'},
-    {'edad': 9, 'sexo': 'M'},
-    {'edad': 12, 'sexo': 'F'},
-    {'edad': 14, 'sexo': 'F'},
     {'edad': 15, 'sexo': 'M'},
-    {'edad': 13, 'sexo': 'F'},
-    {'edad': 10, 'sexo': 'F'},
+    {'edad': 16, 'sexo': 'F'},
+    {'edad': 19, 'sexo': 'M'},
+    {'edad': 16, 'sexo': 'F'},
+    {'edad': 19, 'sexo': 'M'},
+    {'edad': 12, 'sexo': 'F'},
     {'edad': 11, 'sexo': 'F'},
+    {'edad': 15, 'sexo': 'M'},
+    {'edad': 16, 'sexo': 'F'},
+    {'edad': 18, 'sexo': 'F'},
+    {'edad': 15, 'sexo': 'F'},
     {'edad': 16, 'sexo': 'M'},
     {'edad': 10, 'sexo': 'M'},
-    {'edad': 8, 'sexo': 'M'},
-    {'edad': 12, 'sexo': 'F'},
+    {'edad': 18, 'sexo': 'M'},
+    {'edad': 14, 'sexo': 'F'},
     {'edad': 15, 'sexo': 'F'},
     {'edad': 13, 'sexo': 'F'},
-    {'edad': 9, 'sexo': 'M'},
+    {'edad': 19, 'sexo': 'M'},
     {'edad': 16, 'sexo': 'M'},
     {'edad': 15, 'sexo': 'F'},
     {'edad': 12, 'sexo': 'M'},
-    {'edad': 7, 'sexo': 'F'},
+    {'edad': 17, 'sexo': 'F'},
     {'edad': 10, 'sexo': 'F'},
     {'edad': 13, 'sexo': 'F'},
     {'edad': 12, 'sexo': 'M'},
@@ -182,4 +182,32 @@ school = [
     {'name_class': '6A', 'members': studens6, },
 ]
 
+def mean_age(data_school):
+    all_school = 0
+    counter_school = 0
+    age_school = 0
+    print("Escuela Python")
+    print("______________")
+    print("")
+    for cla in data_school:
+        cla_name = cla.get('name_class')
+        cla_students = cla.get('members')
+        counter_student = 0
+        add_student = 0
+        for s in cla_students:
+            counter_student += 1
+            add_student += int(s.get('edad'))
+        mean_student = add_student/counter_student
+        counter_school += 1
+        age_school += mean_student
+        print('Salon: {0}'.format(cla_name))
+        print('Cantidad de Estudiantes: {0}'.format(counter_student))
+        print('Promedio de Edad: {0}'.format(mean_student))
+        print("______________")
+        print("")
+
+    print('Cantidad de salones: {0}'.format(counter_school))
+    print('Promedio de edad global: {0}'.format(age_school/counter_school))
+
+mean_age(school)
 
